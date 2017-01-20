52ee6c8b9a56033d0458e77e6509e3f350dee845
cgarcia <cgarcia@inteliglobe.com>
2017-01-19 19:44:26 -0600

    database user table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,14 +2,29 @@
 
 # --- !Ups
 
-CREATE DATABASE "playshiro"
-    WITH
-    OWNER = icinga
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    CONNECTION LIMIT = -1;
+-- CREATE DATABASE "playshiro"
+--     WITH
+--     OWNER = icinga
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.UTF-8'
+--     CONNECTION LIMIT = -1;
+--
+-- GRANT ALL ON DATABASE "playshiro" TO icinga;
 
-GRANT ALL ON DATABASE "playshiro" TO icinga;
+CREATE TABLE public.subject_data
+(
+    id bigint NOT NULL,
+    nickname character varying(64) NOT NULL,
+    password character varying(256),
+    PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.subject_data
+    OWNER to icinga;
 
 # --- !Downs
 
